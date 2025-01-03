04096166648b7573f55888d2103c4d6cd2ba60cb
Adam Votocek <popvotocek@gmail.com>
2025-01-03 01:43:40 +0100

    Add MIDI controller functionality with potentiometer support

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,40 @@
-#include <Arduino.h>
+// this is a MIDI controller that has 9 potentiometers, based on the ARDUINO PRO MICRO
+// it is meant to expand on the features of akai apc mini mk2
 
-// put function declarations here:
-int myFunction(int, int);
+#include <Arduino.h>
+#include <MIDIUSB.h>
+
+uint8_t analogPins[9] = {A0, A1, A2, A3, A6, A7, A8, A9, A10};
+
+uint8_t firstCC = 70;
+
+uint8_t potentiometerValues[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+uint8_t lastPotentiometerValues[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+void controlChange(byte channel, byte control, byte value)
+{
+    midiEventPacket_t event = {0x0B, static_cast<uint8_t>(0xB0 | channel), control, value};
+    MidiUSB.sendMIDI(event);
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    //Serial.begin(115200);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+    for (int i = 0; i < 9; i++)
+    {
+        potentiometerValues[i] = map(analogRead(analogPins[i]), 0, 1023, 0, 127);
+        if (potentiometerValues[i] != lastPotentiometerValues[i])
+        {
+            controlChange(0, firstCC + i, potentiometerValues[i]);
+            // Serial.print("CC: ");
+            // Serial.print(firstCC + i);
+            // Serial.print(" Value: ");
+            // Serial.println(potentiometerValues[i]);
+            lastPotentiometerValues[i] = potentiometerValues[i];
+        }
+    }
+    MidiUSB.flush();
 }
