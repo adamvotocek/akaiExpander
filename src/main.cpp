f2ebe51662788e6353395ae3bb613694b9f83c16
adamvotocek <popvotocek@gmail.com>
2025-09-08 19:10:09 +0200

    added option to simply enable/disable serial

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,17 @@
 // this is a MIDI controller that has 9 potentiometers, based on the ARDUINO PRO MICRO
 // it is meant to expand on the features of akai apc mini mk2
 
+// Uncomment this if you want to print the CC values to serial
+//#define SERIAL_DEBUG_MODE
+
 #include <Arduino.h>
 #include <MIDIUSB.h>
 
+
 #define NUM_POTS 9
 #define FIRST_CC 70
+
+#define MIDI_BOUNCE_THRESHOLD 4 // how many times a value bounces between two values before we pick one and stop bouncing
 
 uint8_t analogPins[NUM_POTS] = {A0, A1, A2, A3, A6, A7, A8, A9, A10};
 
@@ -14,7 +20,6 @@ uint8_t lastMidiValues[NUM_POTS] = {0};
 uint8_t secondToLastMidiValues[NUM_POTS] = {0};
 
 unsigned long midiBounceCounter[NUM_POTS] = {0};
-uint8_t midiBounceThreshold = 4; // how many times a value bounces between two values before we pick one and stop bouncing
 
 void controlChange(byte channel, byte control, byte value)
 {
@@ -24,7 +29,9 @@ void controlChange(byte channel, byte control, byte value)
 
 void setup()
 {
+#ifdef SERIAL_DEBUG_MODE
     Serial.begin(115200);
+#endif
 }
 
 void loop()
@@ -46,14 +53,16 @@ void loop()
             secondToLastMidiValues[i] = lastMidiValues[i];
             lastMidiValues[i] = midiValues[i];
 
-            if (midiBounceCounter[i] <= midiBounceThreshold)
+            if (midiBounceCounter[i] <= MIDI_BOUNCE_THRESHOLD)
             {
                 controlChange(0, FIRST_CC + i, midiValues[i]);
-                
+
+#ifdef SERIAL_DEBUG_MODE
                 Serial.print("CC: ");
                 Serial.print(FIRST_CC + i);
                 Serial.print(" Value: ");
                 Serial.println(midiValues[i]);
+#endif
             }
         }
     }
